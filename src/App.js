d0041318af182e65b48489b10d261c5d07748885
oliverpople <oliverpople@gmail.com>
2019-04-08 09:42:27 -0700

    Search input now only submits a request on clicking submit button or pressing enter.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,9 @@ export default function App() {
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState("react hooks");
 
-  useEffect(
-    () => {
-      getResults();
-    },
-    [query]
-  );
+  useEffect(() => {
+    getResults();
+  }, []);
 
   const getResults = async () => {
     const response = await axios.get(
@@ -19,20 +16,28 @@ export default function App() {
     setResults(response.data.hits);
   };
 
+  const handleSearch = event => {
+    event.preventDefault();
+    getResults();
+  };
+
   return (
     <>
-      <input
-        type="text"
-        onChange={event => setQuery(event.target.value)}
-        value={query}
-      />
-      <ul>
-        {results.map(result => (
-          <li key={result.objectID}>
-            <a href={result.url}>{result.title}</a>
-          </li>
-        ))}
-      </ul>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          onChange={event => setQuery(event.target.value)}
+          value={query}
+        />
+        <button type="submit">Search</button>
+        <ul>
+          {results.map(result => (
+            <li key={result.objectID}>
+              <a href={result.url}>{result.title}</a>
+            </li>
+          ))}
+        </ul>
+      </form>
     </>
   );
 }
