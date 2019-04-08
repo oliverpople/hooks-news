829162d39a0c5f125d91ca3df257e5964ceff4f8
oliverpople <oliverpople@gmail.com>
2019-04-08 10:27:25 -0700

    Added error handling using hooks.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState("react hooks");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const searchInputRef = useRef();
 
   useEffect(() => {
@@ -13,10 +14,16 @@ export default function App() {
 
   const getResults = async () => {
     setLoading(true);
-    const response = await axios.get(
-      `http://hn.algolia.com/api/v1/search?query=${query}`
-    );
-    setResults(response.data.hits);
+
+    try {
+      const response = await axios.get(
+        `http://hn.algolia.com/api/v1/search?query=${query}`
+      );
+      setResults(response.data.hits);
+    } catch (err) {
+      setError(err);
+    }
+
     setLoading(false);
   };
 
@@ -55,6 +62,7 @@ export default function App() {
           ))}
         </ul>
       )}
+      {error && <div>{error.message}</div>}
     </>
   );
 }
